d547e58d7d7c7016a28a7268e17554b6ae77b62c
Dylan Hays <twentygx@outlook.com>
2023-02-28 19:47:34 -0600

    commit tree 39e91cc7816e8eed539559c8071630ecac006774

---
+++ db/migrate/20230301013353_create_costumes.rb
@@ -0,0 +1,10 @@
+class CreateCostumes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+    end
+  end
+end
